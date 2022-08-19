5c3e4e89fb8b85b150e7a58e4f0d7e628955ee7b
wiktor hanc <wiktor.hc@gmail.com>
2022-08-19 11:38:23 +0200

    Formularz

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submitForm);
